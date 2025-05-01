972017ed679a41547025b267bb7c643147825d21
atul0636@gmail.com <RGAak@0636>
2025-05-01 23:26:03 +0530

    fix test

--- src/test/groovy/com/lykup/authservice/AuthserviceApplicationTests.java
+++ src/test/groovy/com/lykup/authservice/AuthserviceApplicationTests.java
@@ -2,8 +2,10 @@ package com.lykup.authservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuthserviceApplicationTests {
 
 	@Test
